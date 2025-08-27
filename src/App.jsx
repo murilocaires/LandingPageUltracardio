baf1d8722d698a69b996187f77b3eb5e8dc5fd9b
Murilo Caires <mcaires94@gmail.com>
2025-08-27 03:15:27 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Team } from "./components/Team";
 import { Services } from "./components/Services";
 import { Footer } from "./components/Footer";
 import { Contact } from "./components/Contact";
-import "./App.css";
+
 
 function App() {
   return (
